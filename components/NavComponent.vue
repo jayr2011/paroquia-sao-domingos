2a623bf0d004010a91010dbe3078485e2bfbfa4a
Jair Costa dos Santos Junior <jair.santos@compasso.com.br>
2024-09-10 18:13:10 -0300

    adjust responsive mode

--- components/NavComponent.vue
+++ components/NavComponent.vue
@@ -25,12 +25,12 @@ const openDropdown = computed(() => {
 </script>
 
 <template>
-<nav class="fixed w-full z-50 bg-gray-100 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-  <div class="bg-gray-100 max-w-full flex flex-wrap items-center  justify-between mx-auto p-4">
+<nav class=" border border-b-grey- w-full z-50 bg-gray-100 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+  <div class="nav-container bg-gray-100 max-w-full flex flex-wrap items-center  justify-between mx-auto p-4">
     <NuxtLink to="/">
-      <a href="#" class="flex items-center space-x-9 rtl:space-x-reverse">
+      <a href="#" class="title-icon flex items-center rtl:space-x-reverse">
         <img src="../assets/images/pascom.jpeg" class="h-12 rounded-full" alt="Flowbite Logo" />
-        <span class="initial-font self-center text-xl font-semibold whitespace-nowrap dark:text-white">Paroquia São Domingos de Gusmão</span>
+        <span class="initial-font self-center text-xl font-semibold dark:text-white">Paroquia São Domingos de Gusmão</span>
       </a>
     </NuxtLink>
     <button @click="toggleMobileDropdown" data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
@@ -42,7 +42,7 @@ const openDropdown = computed(() => {
     <div :class="[openDropdowMobile ,'w-full md:block md:w-auto']" id="navbar-dropdown">
       <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-yellow dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" class="block py-2 px-3 text-white bg-yellow-700 rounded md:bg-transparent md:text-yellow-700 md:p-0 md:dark:text-yellow-500 dark:bg-yellow-600 md:dark:bg-transparent" aria-current="page"><NuxtLink to="/">Inicio</NuxtLink></a>
+          <a href="#" class="block py-2 px-3 text-black bg-yellow-700 rounded md:bg-transparent md:hover:text-yellow-700 md:p-0 md:dark:text-yellow-500 dark:bg-yellow-600 md:dark:bg-transparent" aria-current="page"><NuxtLink to="/">Inicio</NuxtLink></a>
         </li>
         <li>
             <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-yellow-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent" @click="toggleDropdown">Nossa Paróquia<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -81,7 +81,35 @@ const openDropdown = computed(() => {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&display=swap');
+.nav-container{
+  @media screen and (max-width: 767px) {
+    display: flex !important;
+    flex-direction: column !important;
+    justify-content: center !important;
+  }
+
+  @media screen and (max-width: 972px) {
+    display: flex !important;
+    justify-content: center !important;
+  }
+}
+.title-icon {
+  @media screen and (max-width: 767px) {
+    display: flex !important;
+    flex-direction: column !important;
+  }
+  @media screen and (max-width: 972px) {
+    margin-bottom: 18px;
+  }
+}
+
+.title-icon img {
+  margin-right: 18px;
+}
 .initial-font {
   font-family: 'Cinzel Decorative', serif;
+  @media screen and (max-width: 767px) {
+    word-break: break-word !important;
+  }
 }
 </style>
